45fc5842c66e9d97cd2cd2fbc2c03335490a339a
Amaury Guillermin <amauryguillermin@gmail.com>
2021-03-08 22:09:18 +0100

    Story migration

---
+++ database/migrations/2021_03_08_171120_create_stories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->id();
+            $table->text('file')->nullable();
+            $table->foreignId('adverb_id');
+            $table->foreignId('adjective_id');
+            $table->foreignId('situation_id');
+            $table->foreignId('objective_id');
+            $table->foreignId('solution_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+}
