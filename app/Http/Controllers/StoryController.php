9ee31ecd50ffcb437eee1c56a1a947eea7985fe4
Amaury Guillermin <amauryguillermin@gmail.com>
2021-03-08 22:08:51 +0100

    Post new story route

---
+++ app/Http/Controllers/StoryController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Story;
+use Illuminate\Http\Request;
+
+class StoryController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'adverb' => 'required',
+            'adjective' => 'required',
+            'situation' => 'required',
+            'objective' => 'required',
+            'solution' => 'required'
+        ]);
+
+        $story = new Story();
+        $story->populateWords(
+            $request->only([
+                    'adverb',
+                    'adjective',
+                    'situation',
+                    'objective',
+                    'solution'
+                ])
+        );
+        $story->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Story  $story
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Story $story)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Story  $story
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Story $story)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Story  $story
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Story $story)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Story  $story
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Story $story)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StoryController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/story', [StoryController::class, 'store']);
