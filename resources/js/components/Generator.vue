34a6844456aeaed32a9d9e8c9933b2fa1c428269
Amaury Guillermin <amauryguillermin@gmail.com>
2021-03-10 20:27:51 +0100

    Main vuejs components

---
+++ resources/js/components/Generator.vue
@@ -0,0 +1,40 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+    data () {
+        return {
+            'adverbs': null,
+            'adjectives': null,
+            'situations': null,
+            'objectives': null,
+            'solutions': null
+        }
+    },
+
+    created () {
+        this.fetchRandomSuggestions('adverbs');
+        this.fetchRandomSuggestions('adjectives');
+        this.fetchRandomSuggestions('situations');
+        this.fetchRandomSuggestions('objectives');
+        this.fetchRandomSuggestions('solutions');
+    },
+
+    methods: {
+        fetchRandomSuggestions(type) {
+            const uri = `api/${type}/random`;
+            axios.get(uri)
+                 .then(response => (
+                     this[type] = response
+                 ));
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
