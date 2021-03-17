e1662f4cce42a20e0c0563eb8ec7e4f6b44895a2
Juju <borrelli.julie1104@gmail.com>
2021-03-17 19:41:21 +0100

    Ajout de la factory et du test pour la colonne complément

---
+++ tests/Feature/ApiComplementTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Database\Seeders\ComplementSeeder;
+
+class ApiComplementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_can_fectch_5random_complements()
+    {
+        $this->seed(ComplementSeeder::class);
+        
+        $response = $this->get('/api/complements/random/5');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(5);
+        
+    }
+}
