42772b210de5bd40812ff02f87b4ddddf9443217
Juju <borrelli.julie1104@gmail.com>
2021-03-20 16:17:00 +0100

    Prise en main du code et ajout du scrolling dynamique au clic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
                         <div class="col-8">
                             <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
                             <p class="lead">
-                            <a href="#" class="btn btn-lg btn-secondary fw-bold">Learn more</a>
+                            <a href="#" class="btn btn-lg btn-secondary fw-bold">En savoir plus</a>
                         </div>
                     </div>
                 </div>
