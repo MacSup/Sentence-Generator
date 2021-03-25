4cf23ec834b3239815e06ba235a0d5a390eba7dd
Amaury Guillermin <amauryguillermin@gmail.com>
2021-03-25 11:47:14 +0100

    Adding latest element to the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,14 @@
     <x-slot name="navbar">
         <nav class="navbar navbar-light bg-white fixed-top shadow-sm" id ="navigation">
             <div class="container">
-                <span class="navbar-brand mb-0 h1">CREPIS</span>
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('/images/logo.png') }}" alt="" width="30" height="20" class="d-inline-block align-text-top">
+                    CREPIS
+                </a>
 
                 <ul class="nav nav-pills">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#home">Accueil</a>
+                        <a class="nav-link" href="#about">Nos missions</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#methods">Nos méthodes</a>
@@ -28,7 +31,7 @@
             <header>
                 <div class="overlay"></div>
                 <video playsinline autoplay muted loop>
-                    <source src="{{ asset('/videos/welcome-x2.mp4') }}" type="video/mp4">
+                    <source src="{{ asset('/videos/cleaning-x4.mp4') }}" type="video/mp4">
                 </video>
                 <div class="container h-100">
                     <div class="d-flex h-100 text-center align-items-center">
@@ -60,19 +63,67 @@
         </section>
         <section id="methods">
             <div class="container">
-                <h1>Nos méthodes</h1>
+
+                <div class="row text-center align-items-center">
+                    <video id="methods-vid"autoplay muted loop>
+                        <source src="{{ asset('/videos/push&pull.mp4') }}" type="video/mp4">
+                    </video>
+                </div>
+
             </div>
             
         </section>
         <section id="team">
             <div class="container">
-                <h1>Notre équipe</h1>
+
+                <div class="row">
+                    <div class="col-8">
+                        <img id="team-img" src="{{ asset('/images/team.png') }}" alt="Notre équipe">
+                    </div>
+                    <div class="col-4 text-start">
+                        <p>
+                            <h5>Le CREPIS compte 3 laboratoires de recherche : </h5>
+                            <ul>
+                                <li class="text-success">La Famille Tout à Cramé</li>
+                                <li class="text-danger">Le Droit à la Paresse</li>
+                                <li class="text-info">Ma Sorcière Bien Aimée</li>
+                            </ul>
+                        </p>
+                        <br>
+                        <p>
+                            <b>Louis Billon</b>, 
+                            <b>Morgane Blain</b>, 
+                            <b>Lisa Blanc</b>, 
+                            <b class="text-success">Julie Borrelli</b>, 
+                            <b>Alizée Bouchot</b>, 
+                            <b class="text-info">Mathilde Chénin</b>, 
+                            <b>Loni Cornelis</b>, 
+                            <b>Mariana De Souza</b>, 
+                            <b class="text-danger">David Gauthier</b>, 
+                            <b class="text-info">Caroline Gueye</b>, 
+                            <b class="text-danger">Amaury Guillermin</b>, 
+                            <b>Guillaume Grumel</b>, 
+                            <b class="text-danger">Nawal Guendouz</b>, 
+                            <b class="text-success">Florian Ingels</b>, 
+                            <b class="text-danger">Sylvianne Lathuillière</b>, 
+                            <b class="text-success">Camille Moriot</b>, 
+                            <b class="text-danger">Éric Pardoux</b>, 
+                            <b>Sacha Pasquier</b>, 
+                            <b class="text-success">Anissa Pomies</b>, 
+                            <b class="text-success">Karine Priot</b>, 
+                            <b class="text-info">Stella Ramos-Canut</b>, 
+                            <b>Clara Saleri Lunazzi</b>, 
+                            <b>Emil Tapissier</b>, 
+                            <b class="text-info">Florent Tournus</b>, 
+                            <b>Bruno Yvonnet</b>
+                        </p>
+                    </div>
+                </div>
             </div>
             
         </section>
         <section id="solutions">
             <div class="container">
-                <h1>Nos solutions</h1>
 
                 <div class="row" id="app">
                     <carousel></carousel>
@@ -80,7 +131,7 @@
 
                 <div class="row justify-content-center">
                     <div class="col-md-6 col-lg-4">
-                        <a href="/generator" class="btn btn-lg btn-info fw-bold" role="Button">Générer ma propre solution</a>
+                        <a href="/generator" class="btn btn-lg btn-info fw-bold" role="Button">Participez à l'étude</a>
                     </div>
                     
                 </div>
