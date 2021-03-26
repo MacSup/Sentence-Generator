15f45b8edc5b05c4e38a8e1e6f486d728f7780ef
Amaury Guillermin <amauryguillermin@gmail.com>
2021-03-26 17:58:52 +0100

    Latest change

--- resources/js/components/Generator.vue
+++ resources/js/components/Generator.vue
@@ -84,7 +84,7 @@
             </div>
         </div>
 
-        <div class="modal fade" id="generator-modal" tabindex="-1" aria-labelledby="generatormodal" aria-hidden="true">
+        <div class="modal fade" id="generator-modal" tabindex="-1" aria-labelledby="generator-modal" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                 <div class="modal-header">
@@ -94,23 +94,23 @@
                 <div class="modal-body">
                     <div class="container">
                         <div class="row">
-                            <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
+                            <ul class="nav nav-tabs mb-4" id="tabGenerator" role="tablist">
                                 <li class="nav-item" role="presentation">
                                     <a 
                                         class="nav-link active" 
-                                        id="feeling-tab" 
+                                        id="tab-1" 
                                         data-bs-toggle="tab" 
                                         data-bs-target="#feeling-content" 
                                         type="button" 
                                         role="tab" 
                                         aria-controls="feeling-content" 
                                         aria-selected="true"
-                                    >Sentiment</a>
+                                    >Emotion</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
                                     <a 
                                         class="nav-link" 
-                                        id="situation-tab" 
+                                        id="tab-2" 
                                         data-bs-toggle="tab" 
                                         data-bs-target="#situation-content" 
                                         type="button" 
@@ -122,27 +122,39 @@
                                 <li class="nav-item" role="presentation">
                                     <a 
                                         class="nav-link" 
-                                        id="action-tab" 
+                                        id="tab-3" 
                                         data-bs-toggle="tab" 
                                         data-bs-target="#action-content" 
                                         type="button" 
                                         role="tab" 
                                         aria-controls="action-content" 
                                         aria-selected="false"
-                                    >Action</a>
+                                    >Etat recherché</a>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                    <a 
+                                        class="nav-link" 
+                                        id="tab-4" 
+                                        data-bs-toggle="tab" 
+                                        data-bs-target="#solution-content" 
+                                        type="button" 
+                                        role="tab" 
+                                        aria-controls="solution-content" 
+                                        aria-selected="false"
+                                    >Solution</a>
                                 </li>
                             </ul>
 
                             <div class="tab-content" id="myTabContent">
-                                <div class="tab-pane fade show active" id="feeling-content" role="tabpanel" aria-labelledby="feeling-tab">
+                                <div class="tab-pane fade show active" id="feeling-content" role="tabpanel" aria-labelledby="tab-1">
                                     <div class="row align-items-center">
                                         <div class="col-12 mb-3">
-                                            Choissiez votre ressentiment :  
+                                            Choissiez votre émotion :  
                                         </div>
 
                                         <div class="col-6">
                                             <div class="form-check" v-for="adverb in choices.adverbs" :key="adverb.id">
-                                                <input class="form-check-input" type="checkbox" :value="adverb.id">
+                                                <input class="form-check-input" type="checkbox" :value="adverb.id" v-model="answer.adverb">
                                                 <label class="form-check-label">
                                                     {{ adverb.content }}
                                                 </label>
@@ -150,7 +162,7 @@
                                         </div>
                                         <div class="col-6">
                                             <div class="form-check" v-for="adjective in choices.adjectives" :key="adjective.id">
-                                                <input class="form-check-input" type="checkbox" :value="adjective.id">
+                                                <input class="form-check-input" type="checkbox" :value="adjective.id" v-model="answer.adjective">
                                                 <label class="form-check-label">
                                                     {{ adjective.content }}
                                                 </label>
@@ -158,38 +170,56 @@
                                         </div>
 
                                         <div class="col-12 mt-4">
-                                            Ce que je trouve {suite de la phrase}
+                                            Ce que je trouve {{ selectedAdverb + ' ' + selectedAdjective }}
                                         </div>
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="situation-content" role="tabpanel" aria-labelledby="situation-tab">
+                                <div class="tab-pane fade" id="situation-content" role="tabpanel" aria-labelledby="tab-2">
                                     <div class="row">
                                         <div class="col-12">
 
                                         </div>
                                         <div class="col-12">
                                             <div class="form-check" v-for="situation in choices.situations" :key="situation.id">
-                                                <input class="form-check-input" type="checkbox" :value="situation.id">
+                                                <input class="form-check-input" type="checkbox" :value="situation.id" v-model="answer.situation">
                                                 <label class="form-check-label">
                                                     {{ situation.content }}
                                                 </label>
                                             </div>
                                         </div>
 
+                                        <div class="col-12 mt-4">
+                                            c'est quand {{ selectedSituation }}
+                                        </div>
+
                                     </div>
                                 </div>
-                                <div class="tab-pane fade" id="action-content" role="tabpanel" aria-labelledby="action-tab">
+                                <div class="tab-pane fade" id="action-content" role="tabpanel" aria-labelledby="tab-3">
                                     <div class="row">
                                         <div class="col-2">
 
                                         </div>
                                         <div class="col-10">
                                             <div class="form-check" v-for="objective in choices.objectives" :key="objective.id">
-                                                <input class="form-check-input" type="checkbox" :value="objective.id">
+                                                <input class="form-check-input" type="checkbox" :value="objective.id" v-model="answer.objective">
                                                 <label class="form-check-label">
                                                     {{ objective.content }}
                                                 </label>
                                             </div>
+                                        </div>
+
+                                        <div class="col-12 mt-4">
+                                            du coup, pour {{ selectedObjective }}
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="tab-pane fade" id="solution-content" role="tabpanel" aria-labelledby="tab-4">
+                                    <div class="row">
+
+                                        <div class="col-12 mt-4">
+                                            {{ selectedSolution }}
                                         </div>
 
                                     </div>
@@ -201,8 +231,10 @@
                 <div class="modal-footer">
                     <button 
                         type="button" 
-                        class="btn btn-primary"
-                    >Suivant</button>
+                        class="btn"
+                        :class="modalButtonClass"
+                        v-on:click="buttonModalAction(modalButtonText)"
+                    >{{ modalButtonText }}</button>
                 </div>
                 </div>
             </div>
@@ -212,27 +244,93 @@
 </template>
 
 <script>
+var bootstrap = require('bootstrap')
+
 export default {
 
     data () {
         return {
             isTextAreaDisabled: true,
+            isAnswered: false,
             sentence: null,
-            choices: {
-                'adverbs': null,
-                'adjectives': null,
-                'situations': null,
-                'objectives': null,
-                'solutions': null
+            answer: {
+                adverb: [],
+                adjective: [],
+                situation: [],
+                objective: [],
+                solution: []
             },
-            story : {
+            choices: {
+                adverbs: [],
+                adjectives: [],
+                situations: [],
+                objectives: [],
+                solutions: []
+            },
+            story: {
                 adverb: 0,
                 adjective: 0,
                 situation: 0,
                 objective: 0,
                 solution: 0
-            }
+            },
             
+            
+        }
+    },
+
+    computed: {
+        // Text display
+        selectedAdverb () {
+            return this.getElementContent('adverb')
+        },
+
+        selectedAdjective () {
+            return this.getElementContent('adjective')
+        },
+
+        selectedSituation () {
+            return this.getElementContent('situation')
+        },
+
+        selectedObjective () {
+            return this.getElementContent('objective')
+        },
+
+        selectedSolution () {
+            return this.getElementContent('solution')
+        },
+
+        // Button change
+        modalButtonClass () {
+            if (this.isAnswered) {
+                return 'btn-success'
+            }
+            return 'btn-primary'
+        },
+        modalButtonText () {
+            if (this.isAnswered) {
+                return 'Terminer'
+            }
+            return 'Suivant'
+        }
+    },
+
+    watch: {
+        answer: {
+            handler: function () {
+                let len = [];
+                Object.values(this.answer).forEach(element => {
+                    len.push(element.length);
+                });
+
+                let zIdx = len.find(el => el == 0);
+
+                if (typeof zIdx == "undefined") {
+                    this.isAnswered = true;
+                }
+            },
+            deep: true
         }
     },
 
@@ -242,9 +340,11 @@ export default {
         this.fetchRandomSuggestions('situations', 5);
         this.fetchRandomSuggestions('objectives', 5);
         this.fetchRandomSuggestions('solutions', 1);
+        
     },
 
     methods: {
+        // Page actions
         sentenceAutoGeneration() {
             let adverb = this.getRandomItem('adverb', this.choices.adverbs)
             let adjective = this.getRandomItem('adjective', this.choices.adjectives)
@@ -262,6 +362,62 @@ export default {
             this.sentence = this.generateSentence('{adverbe}', '{adjectif}', '{situation}', '{objectif}', '{solution}');
         },
 
+        sentenceDownload() {
+
+        },
+
+        sentenceShare() {
+
+        },
+
+        // Modal actions
+        buttonModalAction (text) {
+            switch (text) {
+                case 'Suivant':
+                    this.changeModalTabPage()
+                    break;
+                case 'Terminer':
+                    this.closeModal()
+                    break;
+            }
+        },
+
+        changeModalTabPage () {
+            let actualTab = document.querySelector("#tabGenerator li a[aria-selected=true]")
+
+            let tabId = actualTab.id;
+            let id = parseInt(tabId.slice(4));
+
+            if (id == 4) {
+                id = 0
+            }
+
+            let nextTab = document.querySelector('#tabGenerator li a#tab-' + (id + 1))
+
+            let tab = new bootstrap.Tab(nextTab)
+            tab.show()
+        },
+
+        closeModal () {
+            let modal = bootstrap.Modal.getInstance(document.getElementById('generator-modal'))
+            modal.toggle()
+
+            this.story.adverb = this.answer.adverb[0]
+            this.story.adjective = this.answer.adjective[0]
+            this.story.situation = this.answer.situation[0]
+            this.story.objective = this.answer.objective[0]
+            this.story.solution = this.answer.solution[0]
+
+            this.sentence = this.generateSentence(
+                this.selectedAdverb, 
+                this.selectedAdjective, 
+                this.selectedSituation, 
+                this.selectedObjective,
+                this.selectedSolution
+            );
+        },
+
+        // Api calls
         getRandomItem(type, array) {
             let item = array[Math.floor(Math.random() * array.length)];
             console.log(item)
@@ -270,15 +426,19 @@ export default {
             return item.content;
         },
 
-        generateSentence(adverb, adjective, situation, objective, solution) {
-            return `Ce que je trouve ${adverb} ${adjective}, c'est quand ${situation}; du coup, pour me ${objective}, ${solution}`
+        setSolutionItem() {
+            this.answer.solution.push(this.choices.solutions[0].id)
         },
 
         fetchRandomSuggestions(type, number) {
             const uri = `api/${type}/random/${number}`;
             axios.get(uri).then(response => {
                 if (response.status == 200) {
-                    this['choices'][type] = response.data;
+                    this.choices[type] = response.data;
+
+                    if (type == "solutions") {
+                        this.answer.solution.push(response.data[0].id)
+                    }
                 }
             });
         },
@@ -287,10 +447,27 @@ export default {
             axios.post('/story')
         },
 
-        generateBackgroundColor(){ 
+        // Inner page methods
+        generateSentence(adverb, adjective, situation, objective, solution) {
+            return `Ce que je trouve ${adverb} ${adjective}, c'est quand ${situation}; du coup, pour ${objective}, ${solution}`
+        },
+
+        generateBackgroundColor () { 
             return "hsl(" + 360 * Math.random() + ',' +
                     (25 + 70 * Math.random()) + '%,' + 
                     (85 + 10 * Math.random()) + '%)'
+        },
+
+        // "Private" methods
+        getElementContent (type) {
+            let id = this.answer[type][0];
+
+            if (typeof id !== "undefined") {
+                let element = this.choices[type + 's'].find(el => el.id == id);
+                return element.content
+            }
+
+            return ''
         }
     }
 }
