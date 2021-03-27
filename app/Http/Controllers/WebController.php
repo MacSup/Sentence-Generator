c82865e3b69cbcf2d8412bdc08281792a56fead7
Amaury Guillermin <amauryguillermin@gmail.com>
2021-03-27 16:52:35 +0100

    Hotfix : header in media serving

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -20,23 +20,24 @@ class WebController extends Controller
 
     public function images($name)
     {
-        return $this->serveMedia('images', $name);
+        return $this->serveMedia('images', $name, []);
     }
 
     public function videos($name)
     {
-        return $this->serveMedia('videos', $name);
+        return $this->serveMedia('videos', $name, []);
     }
 
     public function stories($name)
     {
-        return $this->serveMedia('stories', $name);
+        $headers = ['Content-Type' =>  'image/svg+xml'];
+        return $this->serveMedia('stories', $name, $headers);
     }
 
-    protected function serveMedia($type, $path)
+    protected function serveMedia($type, $path, $headers)
     {
         if (Storage::disk($type)->exists($path)) {
-            return response()->file(Storage::disk($type)->path($path), ['Content-Type' =>  'image/svg+xml']);
+            return response()->file(Storage::disk($type)->path($path), $headers);
         }
 
         return response('Not found', 404);
